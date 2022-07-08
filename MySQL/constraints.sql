ae02f1bb591be21e053b4b77f3d2879152b94c04
daaviB <britodavi122@gmail.com>
2022-07-07 22:58:28 -0300

    queries MySql

---
+++ MySQL/constraints.sql
@@ -0,0 +1,15 @@
+USE meubanco;
+
+CREATE TABLE pessoas (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    idade INT(100)
+);
+
+CREATE TABLE enderecos (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    rua VARCHAR(255),
+    numero VARCHAR(10), 
+    pessoa_id INT NOT NULL,
+    FOREIGN KEY (pessoa_id) REFERENCES pessoas(id)
+);
---
+++ MySQL/tipo_de_dados.sql
@@ -0,0 +1,15 @@
+-- TIPOS DE DADOS 
+
+-- PRINCIPAIS TIPOS DE DADOS
+
+-- TEXTOS = VARCHAR(X)
+-- NÚMEROS = INT((X)
+-- DATAS = TIMESTAMP, DATE
+
+USE meubanco;
+
+CREATE TABLE pessoas (
+	nome VARCHAR(100),
+    idade INT(100),
+    data_nas DATE
+);
